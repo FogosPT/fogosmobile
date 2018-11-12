7a989afe3b51ef07637931265d2b8217da6f6c10
Guilherme Cabral <eu@guilhermecabral.net>
2018-11-12 18:32:20 +0000

    missing imports

--- lib/reducers/app_reducer.dart
+++ lib/reducers/app_reducer.dart
@@ -1,8 +1,9 @@
 import 'package:fogosmobile/models/app_state.dart';
+import 'package:fogosmobile/reducers/fires_reducer.dart';
 
 AppState appReducer(AppState state, action) {
   return new AppState(
-      isLoading: false,
-      fires: firesReducer(state.fires, action),
+    isLoading: false,
+    fires: firesReducer(state.fires, action),
   );
 }
--- lib/store/app_store.dart
+++ lib/store/app_store.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:redux/redux.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 import 'package:fogosmobile/models/app_state.dart';
+import 'package:fogosmobile/reducers/app_reducer.dart';
 
 final store = new Store<AppState>(
   appReducer,

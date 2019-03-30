226f4f0dfe94d59ddf303d65f854bbb1419058db
Guilherme Cabral <eu@guilhermecabral.net>
2019-03-30 18:10:32 +0000

    feature(notifications): implement FCM notification

--- android/app/src/main/java/com/tomahock/fogos/fogosmobile/MainActivity.java
+++ android/app/src/main/java/com/tomahock/fogos/fogosmobile/MainActivity.java
@@ -1,4 +1,4 @@
-package com.tomahock.fogos.fogosmobile;
+package com.tomahock.fogos;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'package:fogosmobile/styles/theme.dart';
 import 'package:fogosmobile/actions/fires_actions.dart';
@@ -12,8 +13,39 @@ import 'package:fogosmobile/screens/home_page.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  void firebaseCloudMessagingListeners() {
+    if (Platform.isIOS) iOSPermission();
+
+    _firebaseMessaging.getToken().then((token) {
+      print('token');
+      print(token);
+    });
+
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print('on message $message');
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print('on resume $message');
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print('on launch $message');
+      },
+    );
+  }
+
+  void iOSPermission() {
+    _firebaseMessaging.requestNotificationPermissions(
+        IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {});
+  }
+
   @override
   Widget build(BuildContext context) {
+    firebaseCloudMessagingListeners();
+
     return new StoreProvider(
       store: store, // store comes from the app_store.dart import
       child: MaterialApp(
@@ -45,7 +77,10 @@ class MyApp extends StatelessWidget {
                     converter: (Store<AppState> store) => store.state,
                     builder: (BuildContext context, AppState state) {
                       print(state);
-                      if ((state.hasFirstLoad == false || state.hasFirstLoad == null) && (state.isLoading == false || state.isLoading == null)) {
+                      if ((state.hasFirstLoad == false ||
+                              state.hasFirstLoad == null) &&
+                          (state.isLoading == false ||
+                              state.isLoading == null)) {
                         loadFiresAction();
                       }
 

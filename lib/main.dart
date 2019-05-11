bdc4bb7212596ab9e4b44d2a56bd52354c76537a
vanethos <solid.goncalo@gmail.com>
2019-05-11 20:15:14 +0200

    Feature #11 Add ModalBarrier to display loading Add loading to icon when setting notification status Add loading to full screen when loading the map When a bottom sheet is dismissed, the data is removed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'dart:io';
-import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux/redux.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-import 'package:fogosmobile/screens/assets/icons.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fogosmobile/actions/fires_actions.dart';
 import 'package:fogosmobile/actions/preferences_actions.dart';
-import 'package:fogosmobile/store/app_store.dart';
 import 'package:fogosmobile/models/app_state.dart';
+import 'package:fogosmobile/screens/assets/icons.dart';
 import 'package:fogosmobile/screens/home_page.dart';
 import 'package:fogosmobile/screens/settings/settings.dart';
-import 'package:flutter/services.dart';
+import 'package:fogosmobile/store/app_store.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:redux/redux.dart';
 
 import 'middleware/shared_preferences_manager.dart';
 
@@ -22,6 +23,7 @@ const SETTINGS_ROUTE = '/settings';
 
 class MyApp extends StatelessWidget {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
   void firebaseCloudMessagingListeners() {
     if (Platform.isIOS) iOSPermission();
 
@@ -57,88 +59,89 @@ class MyApp extends StatelessWidget {
 class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     SystemChrome.setApplicationSwitcherDescription(
         ApplicationSwitcherDescription(
-          label: "Fogos.pt",
-          primaryColor: Colors.black.value
-        )
-    );
+            label: "Fogos.pt", primaryColor: Colors.black.value));
 
-    return new Scaffold(
-      appBar: new AppBar(
-        backgroundColor: Colors.redAccent,
-        iconTheme: new IconThemeData(color: Colors.white),
-        title: new Text(
-          'Fogos.pt',
-          style: new TextStyle(color: Colors.white),
-        ),
-        actions: [
-          new StoreConnector<AppState, VoidCallback>(
-            converter: (Store<AppState> store) {
-              return () {
-                store.dispatch(new LoadFiresAction());
-                store.dispatch(new LoadAllPreferencesAction());
-              };
-            },
-            builder: (BuildContext context, VoidCallback loadFiresAction) {
-              return new StoreConnector<AppState, AppState>(
-                converter: (Store<AppState> store) => store.state,
-                builder: (BuildContext context, AppState state) {
-                  if ((state.hasFirstLoad == false ||
-                          state.hasFirstLoad == null) &&
-                      (state.isLoading == false || state.isLoading == null) &&
-                      state.fires.length == 0) {
-                    loadFiresAction();
-                  }
-
-                  if (state.isLoading) {
-                    return Container(
-                      width: 54.0,
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: new CircularProgressIndicator(
-                          strokeWidth: 2.0,
-                        ),
-                      ),
-                    );
-                  } else {
-                    return new IconButton(
-                      onPressed: loadFiresAction,
-                      icon: new Icon(Icons.refresh),
-                    );
-                  }
+    return new StoreConnector<AppState, AppState>(
+        converter: (Store<AppState> store) => store.state,
+        builder: (BuildContext context, AppState state) => Scaffold(
+          appBar: new AppBar(
+            backgroundColor: Colors.redAccent,
+            iconTheme: new IconThemeData(color: Colors.white),
+            title: new Text(
+              'Fogos.pt',
+              style: new TextStyle(color: Colors.white),
+            ),
+            actions: [
+              new StoreConnector<AppState, VoidCallback>(
+                converter: (Store<AppState> store) {
+                  return () {
+                    store.dispatch(new LoadFiresAction());
+                    store.dispatch(new LoadAllPreferencesAction());
+                  };
                 },
-              );
-            },
+                builder:
+                    (BuildContext context, VoidCallback loadFiresAction) {
+                  return new StoreConnector<AppState, AppState>(
+                    converter: (Store<AppState> store) => store.state,
+                    builder: (BuildContext context, AppState state) {
+                      if ((state.hasFirstLoad == false ||
+                              state.hasFirstLoad == null) &&
+                          (state.isLoading == false ||
+                              state.isLoading == null) &&
+                          state.fires.length == 0) {
+                        loadFiresAction();
+                      }
+
+                      if (state.isLoading) {
+                        return Container(
+                          width: 54.0,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: new CircularProgressIndicator(
+                              strokeWidth: 2.0,
+                            ),
+                          ),
+                        );
+                      } else {
+                        return new IconButton(
+                          onPressed: loadFiresAction,
+                          icon: new Icon(Icons.refresh),
+                        );
+                      }
+                    },
+                  );
+                },
+              ),
+            ],
           ),
-        ],
-      ),
-      drawer: new Drawer(
-        child: new ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            new DrawerHeader(
-              child: new Center(
-                child: SvgPicture.asset(imgSvgLogoFlame, color: Colors.redAccent),
-              ),
-              decoration: new BoxDecoration(
-                color: Colors.white,
-              ),
+          drawer: new Drawer(
+            child: new ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                new DrawerHeader(
+                  child: new Center(
+                    child: SvgPicture.asset(imgSvgLogoFlame,
+                        color: Colors.redAccent),
+                  ),
+                  decoration: new BoxDecoration(
+                    color: Colors.white,
+                  ),
+                ),
+                new ListTile(
+                  title: new Text('Notificações'),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    Navigator.of(context).pushNamed(SETTINGS_ROUTE);
+                  },
+                  leading: Icon(Icons.settings),
+                ),
+                new Divider(),
+              ],
             ),
-            new ListTile(
-              title: new Text('Notificações'),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.of(context).pushNamed(SETTINGS_ROUTE);
-              },
-              leading: Icon(Icons.settings),
-            ),
-            new Divider(),
-          ],
-        ),
-      ),
-      body: new HomePage(),
-    );
+          ),
+          body: new HomePage(),
+        ));
   }
 }

b012f3feb421d98660aea6ff76e4179380e83571
David Duarte <david_mfra@hotmail.com>
2019-05-21 12:36:41 +0100

    fixed divider.

--- lib/main.dart
+++ lib/main.dart
@@ -201,7 +201,6 @@ class FirstPage extends StatelessWidget {
                   },
                   leading: Icon(Icons.warning),
                 ),
-                new Divider(),
                 new ListTile(
                   title: new Text('Informações'),
                   onTap: () {

2f35dbf3276812dcccbccb552d17d5d9173d421d
vanethos <solid.goncalo@gmail.com>
2019-05-24 16:26:48 +0200

    Updated translations

--- lib/localization/generated/l10n/messages_all.dart
+++ lib/localization/generated/l10n/messages_all.dart
@@ -9,21 +9,21 @@ import 'package:intl/message_lookup_by_library.dart';
 // ignore: implementation_imports
 import 'package:intl/src/intl_helpers.dart';
 
-import 'messages_messages_en.dart' deferred as messages_messages_en;
-import 'messages_messages_pt.dart' deferred as messages_messages_pt;
+import 'messages_en.dart' deferred as messages_en;
+import 'messages_pt.dart' deferred as messages_pt;
 
 typedef Future<dynamic> LibraryLoader();
 Map<String, LibraryLoader> _deferredLibraries = {
-  'messages_en': () => messages_messages_en.loadLibrary(),
-  'messages_pt': () => messages_messages_pt.loadLibrary(),
+  'en': () => messages_en.loadLibrary(),
+  'pt': () => messages_pt.loadLibrary(),
 };
 
 MessageLookupByLibrary _findExact(localeName) {
   switch (localeName) {
-    case 'messages_en':
-      return messages_messages_en.messages;
-    case 'messages_pt':
-      return messages_messages_pt.messages;
+    case 'en':
+      return messages_en.messages;
+    case 'pt':
+      return messages_pt.messages;
     default:
       return null;
   }
--- lib/localization/generated/l10n/messages_en.dart
+++ lib/localization/generated/l10n/messages_en.dart
@@ -18,43 +18,76 @@ typedef MessageIfAbsent(String message_str, List args);
 class MessageLookup extends MessageLookupByLibrary {
   get localeName => 'en';
 
+  static m0(city, id) => "Fire in ${city} https://fogos.pt/fogo/${id}";
+
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static _notInlinedMessages(_) => <String, Function>{
-        "appTitle": MessageLookupByLibrary.simpleMessage("Fogos.pt"),
-        "textAerealMeans": MessageLookupByLibrary.simpleMessage("Aereal Means"),
-        "textCounty": MessageLookupByLibrary.simpleMessage("County"),
-        "textEmptyNotifications": MessageLookupByLibrary.simpleMessage(
-            "You don\'t have any active notifications for Fires"),
-        "textFireStatusArrival":
-            MessageLookupByLibrary.simpleMessage("Ground arrival"),
-        "textFireStatusConclusion":
-            MessageLookupByLibrary.simpleMessage("In conclusion"),
-        "textFireStatusDispatch":
-            MessageLookupByLibrary.simpleMessage("Dispatch"),
-        "textFireStatusDone": MessageLookupByLibrary.simpleMessage("Done"),
-        "textFireStatusFalseAlarm":
-            MessageLookupByLibrary.simpleMessage("False Alarm"),
-        "textFireStatusFalseAlert":
-            MessageLookupByLibrary.simpleMessage("False Alert"),
-        "textFireStatusFirstAlert":
-            MessageLookupByLibrary.simpleMessage("First alert dispatch"),
-        "textFireStatusOngoing":
-            MessageLookupByLibrary.simpleMessage("Ongoing"),
-        "textFireStatusResolution":
-            MessageLookupByLibrary.simpleMessage("In resolution"),
-        "textFireStatusSignificativeOcurrence":
-            MessageLookupByLibrary.simpleMessage("Significative Ocurrence"),
-        "textFireStatusVigilance":
-            MessageLookupByLibrary.simpleMessage("Vigilance"),
-        "textHumanMeans": MessageLookupByLibrary.simpleMessage("Human means"),
-        "textMapboxImprove":
-            MessageLookupByLibrary.simpleMessage("Improve this map"),
-        "textNotifications":
-            MessageLookupByLibrary.simpleMessage("Notifications"),
-        "textInformations":
-            MessageLookupByLibrary.simpleMessage("Informations"),
-        "textStatus": MessageLookupByLibrary.simpleMessage("Status"),
-        "textTerrainMeans":
-            MessageLookupByLibrary.simpleMessage("Terrain means")
-      };
+  static _notInlinedMessages(_) => <String, Function> {
+    "appTitle" : MessageLookupByLibrary.simpleMessage("Fogos.pt"),
+    "textAbout" : MessageLookupByLibrary.simpleMessage("About"),
+    "textAerealMeans" : MessageLookupByLibrary.simpleMessage("Aerial Resources"),
+    "textAerial" : MessageLookupByLibrary.simpleMessage("Aerial"),
+    "textBugs" : MessageLookupByLibrary.simpleMessage("Suggestions/Bug reporting -"),
+    "textCivilProtection" : MessageLookupByLibrary.simpleMessage("Portuguese Civil Protection Agency website"),
+    "textCounty" : MessageLookupByLibrary.simpleMessage("County"),
+    "textDataUpdate" : MessageLookupByLibrary.simpleMessage("Data is updated in 2 minute intervals."),
+    "textEmptyNotifications" : MessageLookupByLibrary.simpleMessage("You don\'t have any active notifications for Fires"),
+    "textFalseAlarm" : MessageLookupByLibrary.simpleMessage("False alarm"),
+    "textFalseAlert" : MessageLookupByLibrary.simpleMessage("False Alert"),
+    "textFireStatusArrival" : MessageLookupByLibrary.simpleMessage("Ground arrival"),
+    "textFireStatusConclusion" : MessageLookupByLibrary.simpleMessage("In conclusion"),
+    "textFireStatusDispatch" : MessageLookupByLibrary.simpleMessage("Dispatch"),
+    "textFireStatusDone" : MessageLookupByLibrary.simpleMessage("Done"),
+    "textFireStatusFalseAlarm" : MessageLookupByLibrary.simpleMessage("False Alarm"),
+    "textFireStatusFalseAlert" : MessageLookupByLibrary.simpleMessage("False Alert"),
+    "textFireStatusFirstAlert" : MessageLookupByLibrary.simpleMessage("First alert dispatch"),
+    "textFireStatusOngoing" : MessageLookupByLibrary.simpleMessage("Ongoing"),
+    "textFireStatusResolution" : MessageLookupByLibrary.simpleMessage("In resolution"),
+    "textFireStatusSignificativeOcurrence" : MessageLookupByLibrary.simpleMessage("Significative Ocurrence"),
+    "textFireStatusVigilance" : MessageLookupByLibrary.simpleMessage("Vigilance"),
+    "textFireman" : MessageLookupByLibrary.simpleMessage("Firemen"),
+    "textFires" : MessageLookupByLibrary.simpleMessage("Fires"),
+    "textHumanMeans" : MessageLookupByLibrary.simpleMessage("Firemen"),
+    "textInformationArrival" : MessageLookupByLibrary.simpleMessage("Arrival to Incident Scene - Resources arrived at incidient location."),
+    "textInformationClosed" : MessageLookupByLibrary.simpleMessage("Closed - All resources are back at their headquarters."),
+    "textInformationClosing" : MessageLookupByLibrary.simpleMessage("Closing - Fire is extinguished, while there could be still some fire spots within incident boundaries"),
+    "textInformationFirstOrderDispatch" : MessageLookupByLibrary.simpleMessage("First alert dispatch - Resources are moving to incident location."),
+    "textInformationIncidentStatus" : MessageLookupByLibrary.simpleMessage("Incident Status"),
+    "textInformationOngoing" : MessageLookupByLibrary.simpleMessage("Ongoing - Fire is ongoing without boundaries"),
+    "textInformationSettling" : MessageLookupByLibrary.simpleMessage("Settling - Fire without risk of propagation beyond boundaries"),
+    "textInformationSupervision" : MessageLookupByLibrary.simpleMessage("Supervision - Resources at incident location to act if needed"),
+    "textInformations" : MessageLookupByLibrary.simpleMessage("Informations"),
+    "textLastNight" : MessageLookupByLibrary.simpleMessage("Last Night"),
+    "textLastNightStatistics" : MessageLookupByLibrary.simpleMessage("Last Night Statistics"),
+    "textLocationApproximate" : MessageLookupByLibrary.simpleMessage("Location may not be exact."),
+    "textMapboxImprove" : MessageLookupByLibrary.simpleMessage("Improve this map"),
+    "textMaximumRisk" : MessageLookupByLibrary.simpleMessage("Maximum"),
+    "textNoConnection" : MessageLookupByLibrary.simpleMessage("We were unnable to connect to the server"),
+    "textNotifications" : MessageLookupByLibrary.simpleMessage("Notifications"),
+    "textNow" : MessageLookupByLibrary.simpleMessage("Now"),
+    "textOther" : MessageLookupByLibrary.simpleMessage("Other"),
+    "textPartners" : MessageLookupByLibrary.simpleMessage("Partners"),
+    "textPreviousDays" : MessageLookupByLibrary.simpleMessage("Last Few Days"),
+    "textRecordsFrom" : MessageLookupByLibrary.simpleMessage("Data collected from"),
+    "textRefreshButton" : MessageLookupByLibrary.simpleMessage("Refresh"),
+    "textResources" : MessageLookupByLibrary.simpleMessage("Resources"),
+    "textRiskHigh" : MessageLookupByLibrary.simpleMessage("High"),
+    "textRiskModerate" : MessageLookupByLibrary.simpleMessage("Moderate"),
+    "textRiskOfFire" : MessageLookupByLibrary.simpleMessage("Risk of Fire"),
+    "textRiskReduced" : MessageLookupByLibrary.simpleMessage("Reduced"),
+    "textRiskVeryHigh" : MessageLookupByLibrary.simpleMessage("Very High"),
+    "textShare" : m0,
+    "textSignificatOccurences" : MessageLookupByLibrary.simpleMessage("Significant Occurrences"),
+    "textStatistics" : MessageLookupByLibrary.simpleMessage("Statistics"),
+    "textStatus" : MessageLookupByLibrary.simpleMessage("Status"),
+    "textTerrainMeans" : MessageLookupByLibrary.simpleMessage("Vehicles"),
+    "textToday" : MessageLookupByLibrary.simpleMessage("Today"),
+    "textTodayDistricts" : MessageLookupByLibrary.simpleMessage("Today\'s District"),
+    "textTodayInterval" : MessageLookupByLibrary.simpleMessage("Today\'s Interval"),
+    "textTotal" : MessageLookupByLibrary.simpleMessage("Total"),
+    "textVehicles" : MessageLookupByLibrary.simpleMessage("Vehicles"),
+    "textWarnings" : MessageLookupByLibrary.simpleMessage("Warnings"),
+    "textYesterday" : MessageLookupByLibrary.simpleMessage("Yesterday"),
+    "textYesterdayDistricts" : MessageLookupByLibrary.simpleMessage("Yesterday\'s Districts"),
+    "textYesterdayInterval" : MessageLookupByLibrary.simpleMessage("Yesterday\'s Interval")
+  };
 }

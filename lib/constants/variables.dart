c13777f518291c315e6a7148b1bb9b75002c91b9
Filipe Barroso <filipe.barroso@gmail.com>
2024-03-02 12:12:17 +0000

    chore:clean up
    Replace some keys with a placeholder

--- lib/constants/variables.dart
+++ lib/constants/variables.dart
@@ -1,13 +1,12 @@
 const double fullPinSize = 50.0;
-const String SENTRY_DSN =
-    "https://2550444359ae439fb491e3e2a4ec6e18@sentry.io/1467956";
-const String MAPBOX_ACCESS_TOKEN =
-    "pk.eyJ1IjoiZm9nb3NwdCIsImEiOiJjbDV0ZXh5Zm0wZTJ1M2NzZGQ3OTI3YTBnIn0.RZf7G6F5sgdhfY4RViVY9g";
-const String MAPBOX_URL_TEMPLATE =
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}";
+const String MAPBOX_ACCESS_TOKEN = "MAPBOX_ACCESS_TOKEN";
 const String MAPBOX_ID = "cjgppvcdp00aa2spjclz9sjst";
-const String MAPBOX_URL_SATTELITE_TEMPLATE =
-    'mapbox://styles/fogospt/cjksgciqsctfg2rp9x9uyh37g';
 // "https://api.mapbox.com/styles/v1/mapbox/$MAPBOX_ID/tiles/256/{z}/{x}/{y}?access_token=$MAPBOX_ACCESS_TOKEN";
 const String MAPBOX_SATTELITE_ID = "satellite-streets-v11";
 const String MAPBOX_TEMPLATE_STYLE = "mapbox://styles/fogospt/$MAPBOX_ID";
+const String MAPBOX_URL_SATTELITE_TEMPLATE =
+    'mapbox://styles/fogospt/cjksgciqsctfg2rp9x9uyh37g';
+const String MAPBOX_URL_TEMPLATE =
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}";
+const String SENTRY_DSN =
+    "https://2550444359ae439fb491e3e2a4ec6e18@sentry.io/1467956";

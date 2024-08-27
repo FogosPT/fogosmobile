6705a61c4f6166ef9f71e7acda685d3a815d2817
Filipe Barroso <filipe.barroso@gmail.com>
2024-08-27 20:21:32 +0100

    chore: remove unused code

--- android/app/src/main/kotlin/com/example/fogosmobile/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tomahock.fogosmobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/tomahock/fogosmobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.tomahock.fogosmobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()

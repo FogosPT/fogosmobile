6d652d9a0c650630bf7707454be21eb2e4fd3e31
Filipe Barroso <filipe.barroso@gmail.com>
2024-08-28 13:58:48 +0100

    feat: add notification cubit

---
+++ apps/fogospt/lib/features/notifications/application/notifications/notification_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:fogospt/features/notifications/application/municipalities_service.dart';
+import 'package:fogospt/features/notifications/application/notifications/notification_state.dart';
+import 'package:fogospt/features/notifications/domain/municipality_data.dart';
+import 'package:get_it/get_it.dart';
+
+class NotificationCubit extends Cubit<NotificationState> {
+  NotificationCubit() : super(NotificationStarted());
+
+  final MunicipalitiesService _municipalitiesService =
+      GetIt.I<MunicipalitiesService>();
+
+  Future<void> fetchNotifications() async {
+    List<Municipality> _municipalities =
+        await _municipalitiesService.fetchAllMunicipalities();
+
+    emit(NotificationLoading());
+    try {
+      emit(NotificationSuccessful(_municipalities));
+    } on Exception catch (e) {
+      emit(NotificationFailed(error: e.toString()));
+    }
+  }
+}
---
+++ apps/fogospt/lib/features/notifications/application/notifications/notification_state.dart
@@ -0,0 +1,33 @@
+import 'package:dart_mappable/dart_mappable.dart';
+import 'package:fogospt/features/notifications/domain/municipality_data.dart';
+
+part 'notification_state.mapper.dart';
+
+@MappableClass()
+sealed class NotificationState with NotificationStateMappable {}
+
+@MappableClass()
+class NotificationStarted extends NotificationState
+    with NotificationStartedMappable {
+  NotificationStarted();
+}
+
+@MappableClass()
+class NotificationLoading extends NotificationState
+    with NotificationLoadingMappable {}
+
+@MappableClass()
+class NotificationSuccessful extends NotificationState
+    with NotificationSuccessfulMappable {
+  final List<Municipality> municipalities;
+  NotificationSuccessful(this.municipalities);
+}
+
+@MappableClass()
+class NotificationFailed extends NotificationState
+    with NotificationFailedMappable {
+  final String error;
+  NotificationFailed({
+    required this.error,
+  });
+}

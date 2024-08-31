c996862da86a7898745038fe5a6581058dffbb31
Filipe Barroso <filipe.barroso@gmail.com>
2024-08-31 23:51:25 +0100

    refactor: improve access l10n

--- apps/fogospt/lib/features/map/presentation/pages/fires_map_page.dart
+++ apps/fogospt/lib/features/map/presentation/pages/fires_map_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:fogos_api/features/latest_warnings/domain/fire.dart';
 import 'package:fogospt/constants/assets.dart';
 import 'package:fogospt/constants/colors.dart';
@@ -49,7 +48,7 @@ class _FiresMapPageState extends State<FiresMapPage> with MessageWatcherBase {
       appBar: AppBar(
         foregroundColor: Colors.white,
         backgroundColor: appFogosOrange,
-        title: Text(AppLocalizations.of(context)!.helloWorld),
+        title: Text(context.l10n.fogospt),
       ),
       drawer: Drawer(
         child: ListView(
--- apps/warnings/lib/utils/extensions/context_extensions.dart
+++ apps/warnings/lib/utils/extensions/context_extensions.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 extension ContextExtensions on BuildContext {
   TextTheme get textTheme => Theme.of(this).textTheme;
+
+  AppLocalizations get l10n => AppLocalizations.of(this)!;
 }

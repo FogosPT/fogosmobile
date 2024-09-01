0849106290c492fd02e753fb6abbe0f099f7e473
Filipe Barroso <filipe.barroso@gmail.com>
2024-09-01 16:36:16 +0100

    refactor: update mappable_state brick

--- mason_bricks/mappable_state/__brick__/{{name.snakeCase()}}.dart
+++ mason_bricks/mappable_state/__brick__/{{name.snakeCase()}}.dart
@@ -2,21 +2,24 @@ import 'package:dart_mappable/dart_mappable.dart';
 
 part '{{name.snakeCase()}}.mapper.dart';
 
-
 @MappableClass()
-sealed class {{name}}State with {{name}}StateMappable {}
+final class {{name}}State extends BaseState with {{name}}StateMappable {
+  final List<String> labels;
 
-@MappableClass()
-class {{name}}Started extends {{name}}State with {{name}}StartedMappable {}
-
-@MappableClass()
-class {{name}}Loading extends {{name}}State with {{name}}LoadingMappable {}
-
-@MappableClass()
-class {{name}}Successful extends {{name}}State with {{name}}SuccessfulMappable {
-
-  {{name}}Successful();
+  {{name}}State({
+    super.status = StateStatus.initial,
+    String? labels,
+  }) : labels = labels ?? [];
 }
 
-@MappableClass()
-class {{name}}Failed extends {{name}}State with {{name}}FailedMappable {}
+extension {{name}}StateExtension on {{name}}State {
+  {{name}}State failure() => copyWith(status: StateStatus.failure);
+  {{name}}State loading() => copyWith(status: StateStatus.loading);
+  {{name}}State success({
+    List<String>? labels,
+  }) =>
+      copyWith(
+        status: StateStatus.success,
+        lables: labels,
+      );
+}
